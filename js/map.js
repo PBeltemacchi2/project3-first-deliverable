0c55a4c6a1e5a0d69ec5ed4aeb30414732dad440
PBeltemacchi2 <113402574+PBeltemacchi2@users.noreply.github.com>
2023-11-14 23:25:50 -0600

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,8 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: { lat: 41.83505630493164, lng: -87.62705993652344 }
+  })
+  
+  
+}
