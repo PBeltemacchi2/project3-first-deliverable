db96f7e28966c563a01542653cc15d67f3db5e1f
PBeltemacchi2 <113402574+PBeltemacchi2@users.noreply.github.com>
2023-11-14 23:38:10 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -4,5 +4,21 @@ function initMap() {
     center: { lat: 41.83505630493164, lng: -87.62705993652344 }
   })
   
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(41.83505630493164, -87.62705993652344), 
+    map: map,
+    title: "Illinois Tech"
+  });
   
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(41.83088, -87.62116), 
+    map: map,
+    title: "McDonald's"
+  });
+  
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(41.83064, -87.63367), 
+    map: map,
+    title: "Chicago White Sox Field"
+  });
 }
